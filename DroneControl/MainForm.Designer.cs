81850c6987919bf27e119e821d09a9b1ff4794f4
Hendrik4.10 <h.mueller@overzone-studio.de>
2016-01-21 09:56:27 +0100

    FlightControl zu Main Form hinzugefügt

--- DroneControl/MainForm.Designer.cs
+++ DroneControl/MainForm.Designer.cs
@@ -42,9 +42,16 @@
             this.resetButton = new System.Windows.Forms.Button();
             this.accelerationLabel = new System.Windows.Forms.Label();
             this.temperatureLabel = new System.Windows.Forms.Label();
+            this.mainViewTabs = new System.Windows.Forms.TabControl();
+            this.manualControlPage = new System.Windows.Forms.TabPage();
+            this.flightControlPage = new System.Windows.Forms.TabPage();
+            this.motorControl1 = new DroneControl.MotorControl();
+            this.flightControl1 = new DroneControl.FlightControl();
             this.headingIndicator = new DroneControl.Avionics.HeadingIndicatorInstrumentControl();
             this.artificialHorizon = new DroneControl.Avionics.AttitudeIndicatorInstrumentControl();
-            this.motorControl1 = new DroneControl.MotorControl();
+            this.mainViewTabs.SuspendLayout();
+            this.manualControlPage.SuspendLayout();
+            this.flightControlPage.SuspendLayout();
             this.SuspendLayout();
             // 
             // ipInfoLabel
@@ -68,10 +75,10 @@
             // 
             // statusArmedLabel
             // 
-            this.statusArmedLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.statusArmedLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.statusArmedLabel.AutoSize = true;
             this.statusArmedLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.statusArmedLabel.Location = new System.Drawing.Point(430, 13);
+            this.statusArmedLabel.Location = new System.Drawing.Point(498, 13);
             this.statusArmedLabel.Name = "statusArmedLabel";
             this.statusArmedLabel.Size = new System.Drawing.Size(57, 13);
             this.statusArmedLabel.TabIndex = 3;
@@ -79,8 +86,8 @@
             // 
             // armToogleButton
             // 
-            this.armToogleButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.armToogleButton.Location = new System.Drawing.Point(565, 8);
+            this.armToogleButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.armToogleButton.Location = new System.Drawing.Point(633, 8);
             this.armToogleButton.Name = "armToogleButton";
             this.armToogleButton.Size = new System.Drawing.Size(75, 23);
             this.armToogleButton.TabIndex = 4;
@@ -94,7 +101,8 @@
             // 
             // calibrateGyroButton
             // 
-            this.calibrateGyroButton.Location = new System.Drawing.Point(191, 140);
+            this.calibrateGyroButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.calibrateGyroButton.Location = new System.Drawing.Point(187, 264);
             this.calibrateGyroButton.Name = "calibrateGyroButton";
             this.calibrateGyroButton.Size = new System.Drawing.Size(23, 23);
             this.calibrateGyroButton.TabIndex = 6;
@@ -106,9 +114,9 @@
             // 
             this.infoPropertyGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.infoPropertyGrid.HelpVisible = false;
-            this.infoPropertyGrid.Location = new System.Drawing.Point(424, 67);
+            this.infoPropertyGrid.Location = new System.Drawing.Point(492, 67);
             this.infoPropertyGrid.Name = "infoPropertyGrid";
-            this.infoPropertyGrid.Size = new System.Drawing.Size(216, 274);
+            this.infoPropertyGrid.Size = new System.Drawing.Size(216, 371);
             this.infoPropertyGrid.TabIndex = 8;
             // 
             // logButton
@@ -126,7 +134,7 @@
             this.stopButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.stopButton.BackColor = System.Drawing.Color.DarkRed;
             this.stopButton.ForeColor = System.Drawing.Color.White;
-            this.stopButton.Location = new System.Drawing.Point(565, 38);
+            this.stopButton.Location = new System.Drawing.Point(633, 38);
             this.stopButton.Name = "stopButton";
             this.stopButton.Size = new System.Drawing.Size(75, 23);
             this.stopButton.TabIndex = 10;
@@ -137,7 +145,7 @@
             // resetButton
             // 
             this.resetButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.resetButton.Location = new System.Drawing.Point(424, 38);
+            this.resetButton.Location = new System.Drawing.Point(492, 38);
             this.resetButton.Name = "resetButton";
             this.resetButton.Size = new System.Drawing.Size(75, 23);
             this.resetButton.TabIndex = 11;
@@ -147,8 +155,9 @@
             // 
             // accelerationLabel
             // 
+            this.accelerationLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.accelerationLabel.AutoSize = true;
-            this.accelerationLabel.Location = new System.Drawing.Point(138, 322);
+            this.accelerationLabel.Location = new System.Drawing.Point(134, 446);
             this.accelerationLabel.Name = "accelerationLabel";
             this.accelerationLabel.Size = new System.Drawing.Size(66, 13);
             this.accelerationLabel.TabIndex = 12;
@@ -156,16 +165,64 @@
             // 
             // temperatureLabel
             // 
+            this.temperatureLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.temperatureLabel.AutoSize = true;
-            this.temperatureLabel.Location = new System.Drawing.Point(13, 322);
+            this.temperatureLabel.Location = new System.Drawing.Point(9, 446);
             this.temperatureLabel.Name = "temperatureLabel";
             this.temperatureLabel.Size = new System.Drawing.Size(67, 13);
             this.temperatureLabel.TabIndex = 13;
             this.temperatureLabel.Text = "Temperature";
             // 
+            // mainViewTabs
+            // 
+            this.mainViewTabs.Controls.Add(this.manualControlPage);
+            this.mainViewTabs.Controls.Add(this.flightControlPage);
+            this.mainViewTabs.Location = new System.Drawing.Point(12, 38);
+            this.mainViewTabs.Name = "mainViewTabs";
+            this.mainViewTabs.SelectedIndex = 0;
+            this.mainViewTabs.Size = new System.Drawing.Size(474, 219);
+            this.mainViewTabs.TabIndex = 14;
+            // 
+            // manualControlPage
+            // 
+            this.manualControlPage.Controls.Add(this.motorControl1);
+            this.manualControlPage.Location = new System.Drawing.Point(4, 22);
+            this.manualControlPage.Name = "manualControlPage";
+            this.manualControlPage.Padding = new System.Windows.Forms.Padding(3);
+            this.manualControlPage.Size = new System.Drawing.Size(466, 193);
+            this.manualControlPage.TabIndex = 0;
+            this.manualControlPage.Text = "Manual Control";
+            this.manualControlPage.UseVisualStyleBackColor = true;
+            // 
+            // flightControlPage
+            // 
+            this.flightControlPage.Controls.Add(this.flightControl1);
+            this.flightControlPage.Location = new System.Drawing.Point(4, 22);
+            this.flightControlPage.Name = "flightControlPage";
+            this.flightControlPage.Padding = new System.Windows.Forms.Padding(3);
+            this.flightControlPage.Size = new System.Drawing.Size(466, 193);
+            this.flightControlPage.TabIndex = 1;
+            this.flightControlPage.Text = "Flight Control";
+            this.flightControlPage.UseVisualStyleBackColor = true;
+            // 
+            // motorControl1
+            // 
+            this.motorControl1.Location = new System.Drawing.Point(6, 3);
+            this.motorControl1.Name = "motorControl1";
+            this.motorControl1.Size = new System.Drawing.Size(364, 92);
+            this.motorControl1.TabIndex = 3;
+            // 
+            // flightControl1
+            // 
+            this.flightControl1.Location = new System.Drawing.Point(-3, 0);
+            this.flightControl1.Name = "flightControl1";
+            this.flightControl1.Size = new System.Drawing.Size(457, 160);
+            this.flightControl1.TabIndex = 0;
+            // 
             // headingIndicator
             // 
-            this.headingIndicator.Location = new System.Drawing.Point(221, 139);
+            this.headingIndicator.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.headingIndicator.Location = new System.Drawing.Point(217, 263);
             this.headingIndicator.Name = "headingIndicator";
             this.headingIndicator.Size = new System.Drawing.Size(175, 175);
             this.headingIndicator.TabIndex = 7;
@@ -173,41 +230,38 @@
             // 
             // artificialHorizon
             // 
-            this.artificialHorizon.Location = new System.Drawing.Point(12, 140);
+            this.artificialHorizon.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.artificialHorizon.Location = new System.Drawing.Point(8, 264);
             this.artificialHorizon.Name = "artificialHorizon";
             this.artificialHorizon.Size = new System.Drawing.Size(175, 175);
             this.artificialHorizon.TabIndex = 5;
             this.artificialHorizon.Text = "attitudeIndicatorInstrumentControl1";
             // 
-            // motorControl1
-            // 
-            this.motorControl1.Location = new System.Drawing.Point(12, 42);
-            this.motorControl1.Name = "motorControl1";
-            this.motorControl1.Size = new System.Drawing.Size(364, 92);
-            this.motorControl1.TabIndex = 2;
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(652, 353);
+            this.ClientSize = new System.Drawing.Size(720, 468);
+            this.Controls.Add(this.mainViewTabs);
             this.Controls.Add(this.temperatureLabel);
             this.Controls.Add(this.accelerationLabel);
             this.Controls.Add(this.resetButton);
             this.Controls.Add(this.stopButton);
             this.Controls.Add(this.logButton);
-            this.Controls.Add(this.infoPropertyGrid);
             this.Controls.Add(this.headingIndicator);
             this.Controls.Add(this.calibrateGyroButton);
             this.Controls.Add(this.artificialHorizon);
             this.Controls.Add(this.armToogleButton);
             this.Controls.Add(this.statusArmedLabel);
-            this.Controls.Add(this.motorControl1);
             this.Controls.Add(this.pingLabel);
             this.Controls.Add(this.ipInfoLabel);
+            this.Controls.Add(this.infoPropertyGrid);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "Drone Control";
+            this.mainViewTabs.ResumeLayout(false);
+            this.manualControlPage.ResumeLayout(false);
+            this.flightControlPage.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -217,7 +271,6 @@
 
         private System.Windows.Forms.Label ipInfoLabel;
         private System.Windows.Forms.Label pingLabel;
-        private MotorControl motorControl1;
         private System.Windows.Forms.Label statusArmedLabel;
         private System.Windows.Forms.Button armToogleButton;
         private Avionics.AttitudeIndicatorInstrumentControl artificialHorizon;
@@ -230,5 +283,10 @@
         private System.Windows.Forms.Button resetButton;
         private System.Windows.Forms.Label accelerationLabel;
         private System.Windows.Forms.Label temperatureLabel;
+        private System.Windows.Forms.TabControl mainViewTabs;
+        private System.Windows.Forms.TabPage manualControlPage;
+        private MotorControl motorControl1;
+        private System.Windows.Forms.TabPage flightControlPage;
+        private FlightControl flightControl1;
     }
 }
--- DroneControl/MainForm.cs
+++ DroneControl/MainForm.cs
@@ -40,6 +40,7 @@ namespace DroneControl
             drone.OnPingChange += Drone_OnPingChange;
             drone.OnDataChange += Drone_OnDataChange;
             motorControl1.UpdateDrone(drone);
+            flightControl1.UpdateDrone(drone);
 
             ipInfoLabel.Text = string.Format(ipInfoLabel.Text, drone.Address);
             UpdatePing();
@@ -71,7 +72,7 @@ namespace DroneControl
         private Form ShowForm(Form form, Func<Form> onClosed)
         {
             if (onClosed == null)
-                throw new ArgumentNullException("onClosed");
+                throw new ArgumentNullException(nameof(onClosed));
 
             if (form == null || form.IsDisposed)
                 form = onClosed();
