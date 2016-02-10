f6fe9aab2f7ec53c0ccd30e73d033235da6e0741
henrik1235 <henrik1235@users.noreply.github.com>
2016-02-10 23:23:37 +0100

    MPU9150 Treiber einbauen (WIP)

--- DroneFirmware/Gyro9150.cpp
+++ DroneFirmware/Gyro9150.cpp
@@ -6,71 +6,28 @@ Gyro9150::Gyro9150(Config* config) : Gyro(config) {
 void Gyro9150::init() {
 	Log::info("Gyro", "init()");
 
-	Wire.begin(SDA, SCL);
+	Wire.begin(SCL, SDA);
+	mpuOK = mpu.init() == IError_None;
 
-	mpu.reset();
-	mpu.initialize();
-	if (mpu.testConnection())
-		Log::error("Gyro", "testConnection() failed!");
+	if (mpuOK)
+		Log::info("Gyro", "Success");
 	else
-		Log::info("Gyro", "testConnection() OK");
-
-	Log::info("Gyro", "dmpInitialize()");
-
-	int result = mpu.dmpInitialize();
-	if (result)
-		Log::error("Gyro", "result: %d", result);
-
-	Log::info("Gyro", "done with init");
-
-	fifoBuffer = (byte*)malloc(sizeof(byte) * mpu.dmpGetFIFOPacketSize());
-
-	mpu.setDMPEnabled(true);
-	mpu.resetFIFO();
+		Log::error("Gyro", "Failure");
 }
 
 void Gyro9150::update() {
-	if (mpu.getIntFIFOBufferOverflowStatus()) {
-		mpu.resetFIFO();
-
-		Log::error("Gyro", "FIFO overflow!");
-		return;
-	}
-
-	if (!mpu.dmpPacketAvailable())
-		return;
-
 	Profiler::begin("Gyro::update()");
-
-	Profiler::begin("getFIFOBytes()");
-	mpu.getFIFOBytes(fifoBuffer, mpu.dmpGetFIFOPacketSize());
-	Profiler::end();
-
-	// Gyro Werte
-	mpu.dmpGetQuaternion(&q, fifoBuffer);
-	mpu.dmpGetGravity(&gravity, &q);
-	mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-
-	// Beschleunigung
-	mpu.dmpGetAccel(&aa, fifoBuffer);
-	mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-
-	yaw = ypr[0];
-	pitch = ypr[1];
-	roll = ypr[2];
-
-	accX = aaReal.x;
-	accY = aaReal.y;
-	accZ = aaReal.z;
-
-	_dirty = true;
+	if (mpuOK)
+		mpu.update(&roll, &pitch, &yaw); // Pitch und Roll umgedreht
 	Profiler::end();
 }
 
 void Gyro9150::reset() {
-	mpu.resetSensors();
+
 }
 
 float Gyro9150::getTemperature() {
-	return mpu.getTemperature() / 340.00 + 36.53;
+	if (mpuOK)
+		return mpu.readTempData() / 340.00 + 36.53;
+	return 0;
 }
--- DroneFirmware/Gyro9150.h
+++ DroneFirmware/Gyro9150.h
@@ -3,24 +3,14 @@
 
 #include "Gyro.h"
 
-#include <Wire/Wire.h>
-#include <MPU9150/MPU9150_9Axis_MotionApps41.h>
-#include <I2Cdev/I2Cdev.h>
+#include "mpu9150.h"
 
 class Gyro9150 : public Gyro
 {
 protected:
 	MPU9150 mpu;
-	byte* fifoBuffer;
-	int fifoOffset;
+	bool mpuOK;
 
-	// orientation/motion vars
-	Quaternion q;           // [w, x, y, z]         quaternion container
-	VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-	VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-	VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-	VectorFloat gravity;    // [x, y, z]            gravity vector
-	float ypr[3];
 public:
 	explicit Gyro9150(Config* config);
 
